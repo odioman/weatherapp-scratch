2fd3ebb183d9ddaafed5f6d9052fb5c71f2ebd97
Thomas Koshy <ttkoshy@gmail.com>
2023-07-23 20:57:12 -0400

    search fully functional

--- app.js
+++ app.js
@@ -3,32 +3,37 @@ const searchBtn = document.querySelector('.search');
 const city = document.querySelector('.city');
 const temp = document.querySelector('.temp');
 const desc = document.querySelector('.description');
-const precip = document.querySelector('.precipitation');
+const humid = document.querySelector('.humidity');
 const wind = document.querySelector('.wind-speed');
 
 searchBtn.addEventListener('click', (event) => {
     event.preventDefault();
+    const input = searchBar.value;
+    fetchWeather(input);
 })
 
-//fetch not working
 async function fetchWeather(input) {
     try {
-        const response = (input) = await fetch(
+        const response = await fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
             + input +
             "&units=imperial&appid=0a24f6082df14108ca78847b83b32791"   
         );
+        console.log(response);
         const data = await response.json()
+        console.log(data);
+        displayWeather(data)
     } catch (err) {
-        alert(err);
+        alert("City does not exist. Please try again");
     }
 }
 
 function displayWeather(data) {
-    city.textContent = data;
-    desc = data.weather[0];
-    temp =  data.main
-    wind = data.wind;
+    city.textContent = data.name;
+    desc.textContent = data.weather.main;
+    temp.textContent = "Temp: " + data.main.temp + " °F";
+    humid.textContent = "Humidity: " + data.main.humidity + "%"
+    wind.textContent = "Wind Speed: " + data.wind.speed + " mph";
 
     
 }
